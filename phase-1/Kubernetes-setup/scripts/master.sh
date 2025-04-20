fb20d94c76120073312bed2a74c9ec7d45b0abc5
anuja2015 <armdevu15@gmail.com>
2025-04-20 18:20:09 +0530

    Create master.sh

---
+++ phase-1/Kubernetes-setup/scripts/master.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo kubeadm init --pod-network-cidr=172.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
