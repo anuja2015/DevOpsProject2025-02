fa4d9b0a32518e731ddebce253323e2a3a1b3bfa
anuja2015 <armdevu15@gmail.com>
2025-04-20 17:26:39 +0530

    Delete phase-1/Terraform/network-module directory

--- phase-1/Terraform/network-module/output.tf
+++
@@ -1,9 +0,0 @@
-output "subnet_id" {
-  description = "The ID of the subnet"
-  value       = azurerm_subnet.project02-subnet01.id
-}
-
-output "nsg_id" {
-  description = "The ID of the network security group"
-  value       = azurerm_network_security_group.project02-nsg.id
-}
